32d99341c6d818ab33979267c705ed97265bd1c2
abouzarmofidian <a.mofidian69@gmail.com>
2023-05-30 17:49:38 +0330

    prometheus

--- 3-prometheus/1-prometheus.tf
+++ 3-prometheus/1-prometheus.tf
@@ -1,15 +1,15 @@
 #helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 #helm repo update
 
-#helm install prometheus -n monitoring --namespace --create-namespace prometheus-community/prometheus
+#helm install prometheus -n monitoring --namespace --create-namespace prometheus-community/kube-prometheus-stack
 
 resource "helm_release" "prometheus" {
   name = "my-prometheus"
 
   repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "prometheus"
+  chart            = "kube-prometheus-stack"
   namespace        = "monitoring"
   create_namespace = true
-  version          = "22.6.2"
+  #version          = "22.6.2"
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Terraform-Kubernetes
 # 1 --> Deploy Wordpress With Terraform
 # 2 --> Istio
+# 3 --> Prometheus && Grafana
